73cb5db7ce2559f7030b904acc84cefdea9142bb
Nicolas TRANNOIS <nicolas.trannois@sii.fr>
2022-05-18 18:10:57 +0200

    Specify type normally

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,23 @@ function main(): void {
 	const eltPlot = document.querySelector<HTMLDivElement>("#plotlyPlot");
 	if (eltPlot == null) {return;}
 
-	const data = {
+	const data: Partial<Plotly.PlotData> = {
 		type: "scatter",
 		x: allData.x,
 		y: allData.y,
 		mode: "lines+markers",
 		hoverinfo: "text",
 		marker: {color: "#00ff00"}
-	} as Plotly.PlotData;
+	};
 	
-	const layout = {
+	const layout: Partial<Plotly.Layout> = {
 		title: "Just a dumb title",
 		xaxis: {title: "X axis title"},
 		yaxis: {title: "Y axis title"},
 		margin: {t: 50, b: 60, l: 70, r: 40} // 100,80,80,80 by default
-	} as Plotly.Layout;
+	};
 	
-	const config = {responsive: true} as Plotly.Config;
+	const config: Partial<Plotly.Config> = {responsive: true};
 	Plotly.react(eltPlot, [data], layout, config);
 }
 
